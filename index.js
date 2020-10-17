977178a95eff9e7849fa9f69c041f6f80d113fed
Ayushman Gupta <ayushman1998.1998@gmail.com>
2020-10-17 17:39:23 +0530

    added variables for easy usage

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const fs = require("fs");
 const pup = require("puppeteer");
-// for (let i = 1; i <= 10; i++) {
+const URL = "https://www.goodreads.com/quotes/tag/cosmos?page=";
+const pages = 10;
+const fileName = "quotesData.js"
+
+for (let page = 1; page <= pages; page++) {
   (async () => {
     let quotes = [];
     const browser = await pup.launch();
     const page = await browser.newPage();
-    await page.goto("https://www.goodreads.com/quotes/tag/cosmos?page=1", {
+    await page.goto(URL + page, {
       waitUntil: "networkidle2",
+      timeout: 0,
     });
     const rawQuotes = await page.evaluate(() => {
       let result = [];
@@ -26,18 +31,18 @@ const pup = require("puppeteer");
       quotes.push(obj);
     });
 
-   for(let i = 0; i <= rawQuotes[1].length; i++) {
-     console.log(quotes[i])
-    //  quotes[i].author = rawQuotes[1][i];
-   }
-   console.log(quotes)
+    for (let i = 0; i < rawQuotes[1].length; i++) {
+      quotes[i].author = rawQuotes[1][i];
+    }
+    console.log(quotes);
 
-    let quotesStringify = JSON.stringify(quotes);
+    let quotesStringify = JSON.stringify(quotes) + ",";
     quotesStringify = quotesStringify.replace(/\[/g, "");
     quotesStringify = quotesStringify.replace(/\]/g, "");
 
-    // fs.appendFile("quotesData.js", quotesStringify, "utf8", () => {
-    //   console.log("Quotes added");
-    // });
+    fs.appendFile(fileName, quotesStringify, "utf8", () => {
+      console.log("Quotes added");
+    });
     await browser.close();
   })();
+}
